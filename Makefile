28b40cbfe4ce0efe3b46ecd509fe4ec4ecb28485
Ram <jack81306@yahoo.com.tw>
2017-03-20 22:59:19 +0800

    Add avx method and implement

--- Makefile
+++ Makefile
@@ -1,14 +1,15 @@
-CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra
+CFLAGS = -msse2 --std gnu99 -O0 -Wall -Wextra -mavx2
 
 GIT_HOOKS := .git/hooks/applied
 
 all: $(GIT_HOOKS) main.c
-	$(CC) $(CFLAGS) -DSSEPREFETCH -DSSE -DNAIVE -o main main.c
+	$(CC) $(CFLAGS) -DNAIVE -o main main.c
 
 allsp: $(GIT_HOOKS) main.c
 	$(CC) $(CFLAGS) -DSSEPREFETCH  -o sse_prefetch main.c
 	$(CC) $(CFLAGS) -DSSE -o sse main.c
 	$(CC) $(CFLAGS) -DNAIVE -o naive main.c
+	$(CC) $(CFLAGS) -DAVX -o avx main.c
 
 $(GIT_HOOKS):
 	@scripts/install-git-hooks
@@ -19,6 +20,7 @@ tofile: allsp
 	./naive done >> data.txt 
 	./sse done >> data.txt
 	./sse_prefetch done >> data.txt
+	./avx done >> data.txt
 
 clean:
-	$(RM) main sse_prefetch sse naive data.txt
+	$(RM) main sse_prefetch sse naive avx  data.txt
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <assert.h>
 
 #include <xmmintrin.h>
-
+#include <immintrin.h>
 #ifndef TEST_W
 #define TEST_W 4096
 #endif
@@ -20,6 +20,8 @@
  */
 
 #include "impl.c"
+
+
 
 static long diff_in_us(struct timespec t1, struct timespec t2)
 {
@@ -36,66 +38,23 @@ static long diff_in_us(struct timespec t1, struct timespec t2)
 
 int main()
 {
-    /* verify the result of 4x4 matrix */
-    {
-        int testin[16] = { 0, 1,  2,  3,  4,  5,  6,  7,
-                           8, 9, 10, 11, 12, 13, 14, 15
-                         };
-        int testout[16];
-        int expected[16] = { 0, 4,  8, 12, 1, 5,  9, 13,
-                             2, 6, 10, 14, 3, 7, 11, 15
-                           };
-
-        /* for (int y = 0; y < 4; y++) {
-              for (int x = 0; x < 4; x++)
-                  printf(" %2d", testin[y * 4 + x]);
-              printf("\n");
-          }
-          printf("\n");*/
-        sse_transpose(testin, testout, 4, 4);
-        /* for (int y = 0; y < 4; y++) {
-             for (int x = 0; x < 4; x++)
-                 printf(" %2d", testout[y * 4 + x]);
-             printf("\n");
-         }*/
-        assert(0 == memcmp(testout, expected, 16 * sizeof(int)) &&
-               "Verification fails");
-    }
-
     {
         struct timespec start, end;
         int *src  = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out0 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out1 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
-        int *out2 = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
+        int *out = (int *) malloc(sizeof(int) * TEST_W * TEST_H);
 
         srand(time(NULL));
         for (int y = 0; y < TEST_H; y++)
             for (int x = 0; x < TEST_W; x++)
                 *(src + y * TEST_W + x) = rand();
-#ifdef SSEPREFETCH
         clock_gettime(CLOCK_REALTIME, &start);
-        sse_prefetch_transpose(src, out0, TEST_W, TEST_H);
+        transpose(src, out, TEST_W, TEST_H);
         clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse prefetch: \t %ld us\n", diff_in_us(start, end));
-#endif
-#ifdef SSE
-        clock_gettime(CLOCK_REALTIME, &start);
-        sse_transpose(src, out1, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("sse: \t\t %ld us\n", diff_in_us(start, end));
-#endif
-#ifdef NAIVE
-        clock_gettime(CLOCK_REALTIME, &start);
-        naive_transpose(src, out2, TEST_W, TEST_H);
-        clock_gettime(CLOCK_REALTIME, &end);
-        printf("naive: \t\t %ld us\n", diff_in_us(start, end));
-#endif
+        printf("%ld us\n", diff_in_us(start, end));
         free(src);
-        free(out0);
-        free(out1);
-        free(out2);
+        free(out);
     }
 
     return 0;
+
 }
